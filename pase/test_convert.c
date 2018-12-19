1ba82a9f7866f5f597651f69f5f5bebcd8daa5d3
LiYuTJUFE <liyu@tjufe.edu.cn>
2018-12-19 23:03:49 +0800

    test a lot for AMG in PETSC

--- pase/test_convert.c
+++ pase/test_convert.c
@@ -17,60 +17,119 @@
 #include <stdio.h>
 #include "pase_convert.h"
 
-#include "HYPRE_parcsr_ls.h"
-#include "temp_multivector.h"
-#include "_hypre_parcsr_mv.h"
 #include "_hypre_parcsr_ls.h"
-#include "HYPRE_utilities.h"
 
 
 void GetPetscMat(Mat *A, PetscInt n, PetscInt m);
 void GetMultigridMatFromHypreToPetsc(Mat **A_array, Mat **P_array, HYPRE_Int *num_levels, HYPRE_ParCSRMatrix hypre_parcsr_mat);
-static char help[] = "Test Matrix Convert.\n";
+static char help[] = "Test Matrix Convert and AMG.\n";
 /* 
  *  Description:  测试HYPRE矩阵与PETSC矩阵的互相转化
  */
    int
 main ( int argc, char *argv[] )
 {
+   /* PetscInitialize */
    PetscInitialize(&argc,&argv,(char*)0,help);
    PetscErrorCode ierr;
    PetscMPIInt rank;
    PetscViewer viewer;
    MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 
+   /* 测试矩阵声明 */
    Mat                petsc_mat_A;
    HYPRE_IJMatrix     hypre_ij_mat;
    HYPRE_ParCSRMatrix hypre_parcsr_mat;
-   PetscInt n = 4, m = 4;
-
+   PetscInt n = 7, m = 3, row_start, row_end, col_start, col_end;
+   /* 得到一个PETSC矩阵 */
    GetPetscMat(&petsc_mat_A, n, m);
-   MatView(petsc_mat_A, viewer);
-
+//   MatView(petsc_mat_A, viewer);
+   /* 将PETSC矩阵转化为HYPRE矩阵 */
    MatrixConvertPETSC2HYPRE(&hypre_ij_mat, petsc_mat_A);
 //   HYPRE_IJMatrixPrint(hypre_ij_mat, "./hypre_ij_mat");
-
    HYPRE_IJMatrixGetObject(hypre_ij_mat, (void**) &hypre_parcsr_mat);
-
-
+   /* 声明一系列矩阵和向量 */
    Mat *A_array, *P_array;
-   HYPRE_Int idx, num_levels = 2;
+   Vec *U_array, *F_array;
+   HYPRE_Int idx, j, num_levels = 2;
+   /* 利用HYPRE矩阵通过AMG得到一系列PETSC矩阵, 以及插值矩阵 */
    GetMultigridMatFromHypreToPetsc(&A_array, &P_array, &num_levels, hypre_parcsr_mat);
-
+   /* 打印一系列矩阵 */
+   PetscPrintf(PETSC_COMM_WORLD, "A_array\n");
    for (idx = 0; idx < num_levels; ++idx)
    {
+//      MatGetOwnershipRange(A_array[idx], &row_start, &row_end);
+//      MatGetOwnershipRangeColumn(A_array[idx], &col_start, &col_end);
+//      printf("%d: row_start = %d, row_end = %d, col_start = %d, col_end = %d\n", 
+//	    rank, row_start, row_end, col_start, col_end);
+      PetscPrintf(PETSC_COMM_WORLD, "idx = %d\n", idx);
       MatView(A_array[idx], viewer);
    }
+   PetscPrintf(PETSC_COMM_WORLD, "P_array\n");
    for (idx = 0; idx < num_levels-1; ++idx)
    {
+      PetscPrintf(PETSC_COMM_WORLD, "idx = %d\n", idx);
+//      MatGetOwnershipRange(P_array[idx], &row_start, &row_end);
+//      MatGetOwnershipRangeColumn(P_array[idx], &col_start, &col_end);
+//      printf("%d: row_start = %d, row_end = %d, col_start = %d, col_end = %d\n", 
+//	    rank, row_start, row_end, col_start, col_end);
       MatView(P_array[idx], viewer);
    }
+   /* 通过一系列矩阵生成对应的一系列向量 */
+   U_array = malloc(sizeof(Vec)*num_levels);
+   F_array = malloc(sizeof(Vec)*num_levels);
+   for (idx = 0; idx < num_levels; ++idx)
+   {
+      MatCreateVecs(A_array[idx], PETSC_NULL, U_array+idx);
+      MatCreateVecs(A_array[idx], F_array+idx, PETSC_NULL);
+      VecGetOwnershipRange(U_array[idx], &row_start, &row_end);
+      for (j = row_start; j < row_end; ++j)
+      {
+	 VecSetValue(U_array[idx], j, 1.0, INSERT_VALUES);
+      }
+      VecAssemblyBegin(U_array[idx]);
+      VecAssemblyEnd(U_array[idx]);
+   }
+   /* 测试矩阵向量乘 F[idx] = A[idx] U[idx] */
+   for (idx = 0; idx < num_levels; ++idx)
+   {
+      PetscPrintf(PETSC_COMM_WORLD, "F[%d] = A[%d] U[%d]\n", idx, idx, idx);
+      MatMult(A_array[idx], U_array[idx], F_array[idx]);
+      VecView(F_array[idx], viewer);
+   }
+   /* 测试向量插值 U[idx-1] = P[idx-1] U[idx] */
+   for (idx = num_levels-1; idx > 0 ; --idx)
+   {
+      PetscPrintf(PETSC_COMM_WORLD, "U[%d] = P[%d] U[%d]\n", idx-1, idx-1, idx);
+      MatMult(P_array[idx-1], U_array[idx], U_array[idx-1]);
+      VecView(U_array[idx-1], viewer);
+   }
+   /* 测试矩阵向量乘 F[idx] = A[idx] U[idx] */
+   for (idx = 0; idx < num_levels; ++idx)
+   {
+      PetscPrintf(PETSC_COMM_WORLD, "F[%d] = A[%d] U[%d]\n", idx, idx, idx);
+      MatMult(A_array[idx], U_array[idx], F_array[idx]);
+      VecView(F_array[idx], viewer);
+   }
+   /* 测试向量限制 U[idx+1] = P[idx]' U[idx] */
+   for (idx = 0; idx < num_levels-1; ++idx)
+   {
+      PetscPrintf(PETSC_COMM_WORLD, "U[%d] = P[%d]' U[%d]\n", idx+1, idx, idx);
+      MatMultTranspose(P_array[idx], U_array[idx], U_array[idx+1]);
+      VecView(U_array[idx+1], viewer);
+   }
 
+
+   /* 销毁矩阵向量 */
    for (idx = 0; idx < num_levels; ++idx)
    {
       ierr = MatDestroy(&A_array[idx]);
+      ierr = VecDestroy(&U_array[idx]);
+      ierr = VecDestroy(&F_array[idx]);
    }
-   free(A_array);
+   free(A_array); 
+   free(U_array); 
+   free(F_array); 
    for (idx = 0; idx < num_levels-1; ++idx)
    {
       ierr = MatDestroy(&P_array[idx]);
@@ -78,7 +137,7 @@ main ( int argc, char *argv[] )
    free(P_array);
    HYPRE_IJMatrixDestroy(hypre_ij_mat);
    ierr = MatDestroy(&petsc_mat_A);
-
+   /* PetscFinalize */
    ierr = PetscFinalize();
    return 0;
 }
